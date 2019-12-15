4bcf46dd0760673466c4f23e6dee1e12c56a9270
Your Name <you@example.com>
2019-12-15 22:21:18 +0200

    final step - delete reply - added

--- com.kris.tweet/SeleniumTweet.java
+++ com.kris.tweet/SeleniumTweet.java
@@ -37,18 +37,18 @@ public class SeleniumTweet {
 
         //  CLICK BUTTON
         driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[1]/div[1]/form/input[1]")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
         // Go to delete last message
-        driver.findElement(By.partialLinkText("https://twitter.com/865434test/status/" + id)).click();
-        //  id=caret
-//    ele.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);;
-//     //   driver.findElement(By.("div.r-daml9f > span:nth-child(1)")).click();
-//        driver.findElement(By.cssSelector("#react-root > div > div > div.r-1d2f490.r-u8s1d.r-zchlnj.r-ipm5af.r-184en5c > div > div > div:nth-child(2) > div.css-1dbjc4n.r-14lw9ot.r-1f0042m.r-1upvrn0.r-1ekmkwe.r-1udh08x.r-u8s1d > div > div > div > div:nth-child(1)")).click();
+        driver.get("https://twitter.com/865434test/status/" + id + "/");
+        driver.findElement(By.cssSelector("#react-root > div > div > div > main > div > div > div > div > div > div:nth-child(2) > div > section > div > div > div > div:nth-child(3) > div > article > div > div:nth-child(2) > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-5f2r5o.r-1mi0q7o > div:nth-child(1) > div.css-1dbjc4n.r-18u37iz.r-1h0z5md.r-1joea0r > div > div")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/div[3]/div/div/div/div[1]")).click();
+//        driver.findElement(By.xpath("//*[@id=\"react-root\"]/div/div/div/main/div/div/div/div/div/div[2]/div/section/div/div/div/div[3]/div/article/div/div[2]/div[2]/div[1]/div[2]/div")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/div/div/div[1]/div/div/div/div/div[2]/div[2]/div[3]/div[2]")).click();
+
+        driver.quit();
     }
-    //driver.quit();
 }
-//}
-//#react-root > div > div > div > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-1tlfku8.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > section > div > div > div > div:nth-child(1) > div > article > div > div:nth-child(2) > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-5f2r5o.r-1mi0q7o > div.css-1dbjc4n.r-18u37iz.r-1wtj0ep.r-zl2h9q > div.css-1dbjc4n.r-18u37iz.r-1h0z5md.r-1joea0r > div > div > div > div
-//
-////*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[4]/div/div/section/div/div/div/div[1]/div/article/div/div[2]/div[2]/div[1]/div[2]/div/div/div/div
