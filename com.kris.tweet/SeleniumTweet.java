3dca4ed77abc4723be8d12b3c2a44079c209e7c3
Your Name <you@example.com>
2019-12-15 13:38:06 +0200

    added login feature

--- com.kris.tweet/SeleniumTweet.java
+++ com.kris.tweet/SeleniumTweet.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
-
 public class SeleniumTweet {
 
     WebDriver driver;
@@ -16,21 +15,25 @@ public class SeleniumTweet {
     public void startBrowser() {
         System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver-v0.26.0-win64\\geckodriver.exe");
         driver = new FirefoxDriver();
-        driver.get("https://twitter.com/login");
+        //GET URL
+        driver.get("https://twitter.com/");
         // ENTER USERNAME
-        driver.findElement(By.name("session[username_or_email]")).sendKeys("865434test");
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[1]/div[1]/form/div[1]/input")).sendKeys("@865434test");
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        // ENTER PASSWORD
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[1]/div[1]/form/div[2]/input")).click();
+//        driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[1]/div[1]/form/div[2]/input")).clear();
+        driver.findElement(By.xpath("//*[@id=\"doc\"]/div/div[1]/div[1]/div[1]/form/div[2]/input")).sendKeys("e446108");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        //ENTER PASSWORD
-        driver.findElement(By.className("js-password-field")).sendKeys("testing123");
+        //CLICK BUTTON
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[1]/div[1]/form/input[1]")).click();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        driver.findElement(By.className("submit EdgeButton EdgeButton--primary EdgeButtom--medium")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        String expectedUrl= driver.getCurrentUrl();
-        String actualUrl = "https://twitter.com/login";
-        if(actualUrl.equalsIgnoreCase(expectedUrl)) {
-            System.out.println("Test passed"); } else { System.out.println("Test failed"); }
+        //GET HOME SCREEN
+//        String expectedUrl= driver.getCurrentUrl();
+//        String actualUrl =
+//                "https://twitter.com/home";
+//        if(actualUrl.equals(expectedUrl)) {
+//            System.out.println("Test passed"); } else { System.out.println("Test failed"); }
+//    }
     }
 }
--- com.kris.tweet/TheTweet.java
+++ com.kris.tweet/TheTweet.java
@@ -1,41 +1,37 @@
 package com.kris.tweet;
 
-import com.kris.tweet.SeleniumTweet;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.examples.tweets.RetweetStatus;
 
-import java.lang.reflect.Array;
-
 
 public class TheTweet{
     private static Object Status;
     private static long id;
+    //Selenium support
     WebDriver driver;
     private SeleniumTweet sDriver;
-//obj.startBrowser();
 
     public static void main(String[] args) throws TwitterException {
         FirstTweet();
         Retweet();
-        // TODO Auto-generated catch block
+        //Selenium support
         SeleniumTweet sDrive = new SeleniumTweet();
         sDrive.startBrowser();
     }
     public static long FirstTweet() {
         Twitter twitter = TwitterFactory.getSingleton();
         //makes an instance via connecting and authorization
-        String tweet = "Hello, just a tweeting testing again and again and now again too10";
+        String tweet = "Hello, just a tweeting testing again and again and now again too43";
         Status status = null;
-//        String[] statusReturn = null;
         try {
           status = twitter.updateStatus(tweet);
             //makes the first Tweet
-            System.out.printf("Successfully updated status to: " + status.getText() + status.getId());
+            System.out.printf("Successfully updated status to: " + status.getText() +"id:" + status.getId());
         } catch (TwitterException e) {
             e.printStackTrace();
         }
@@ -46,9 +42,9 @@ public class TheTweet{
         Twitter twitter = TwitterFactory.getSingleton();
         try {
             Status status = twitter.retweetStatus(id);
-          //  makes Retweet of the given id
-          System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText() + status.getId());
-            // peint the FistTweet and Retweet
+          //  makes Retweet based on id
+          System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText() +"id:"+ status.getId());
+            // prints the Usernname, FistTweet and Retweet+id
         } catch (TwitterException e) {
             e.printStackTrace();
         }
